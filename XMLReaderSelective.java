81f4942ad2af9438dadb0a9687054260f7c249c5
Mthabisi Sibanda <g23s6705@campus.ru.ac.za>
2025-04-16 21:05:58 +0200

    Change your Java application to only output user-selected fields from that XML file.

--- XMLReaderSelective.java
+++ XMLReaderSelective.java
@@ -4,9 +4,13 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
-public class XMLReader {
+public class XMLReaderSelective {
     public static void main(String[] args) {
         try {
             File inputFile = new File("data.xml"); // Replace "data.xml" with your actual file name
@@ -14,6 +18,11 @@ public class XMLReader {
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter the fields to display (comma-separated): ");
+            String selectedFieldsInput = reader.readLine();
+            List<String> selectedFields = Arrays.asList(selectedFieldsInput.split(",\\s*"));
 
             NodeList recordList = doc.getElementsByTagName("record");
 
@@ -25,11 +34,14 @@ public class XMLReader {
                 for (int j = 0; j < childNodes.getLength(); j++) {
                     if (childNodes.item(j).getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
                         Element element = (Element) childNodes.item(j);
-                        System.out.println(element.getTagName() + ": " + element.getTextContent());
+                        if (selectedFields.contains(element.getTagName())) {
+                            System.out.println(element.getTagName() + ": " + element.getTextContent());
+                        }
                     }
                 }
                 System.out.println();
             }
+            reader.close();
 
         } catch (Exception e) {
             e.printStackTrace();
