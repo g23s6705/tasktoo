ccd3afa46d0fbc844933a28bf06d4bea1d1ad9ff
Mthabisi Sibanda <g23s6705@campus.ru.ac.za>
2025-04-16 21:10:17 +0200

    Changed to a JSON format

--- XMLToJsonConverter.java
+++ XMLToJsonConverter.java
@@ -4,13 +4,14 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
-public class XMLReaderSelective {
+public class XMLToJsonConverter {
     public static void main(String[] args) {
         try {
             File inputFile = new File("data.xml"); // Replace "data.xml" with your actual file name
@@ -28,18 +29,18 @@ public class XMLReaderSelective {
 
             for (int i = 0; i < recordList.getLength(); i++) {
                 Element record = (Element) recordList.item(i);
+                JSONObject jsonRecord = new JSONObject();
                 NodeList childNodes = record.getChildNodes();
 
-                System.out.println("--- Record " + (i + 1) + " ---");
                 for (int j = 0; j < childNodes.getLength(); j++) {
                     if (childNodes.item(j).getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
                         Element element = (Element) childNodes.item(j);
                         if (selectedFields.contains(element.getTagName())) {
-                            System.out.println(element.getTagName() + ": " + element.getTextContent());
+                            jsonRecord.put(element.getTagName(), element.getTextContent());
                         }
                     }
                 }
-                System.out.println();
+                System.out.println(jsonRecord.toString(2)); // Use toString(2) for pretty printing
             }
             reader.close();
 
